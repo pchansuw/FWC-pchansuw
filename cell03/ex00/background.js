daf6f91a00b8cf32d434985bd5f9310c07f16393
pchansuw <pchansuw@student.42bangkok.com>
2026-09-19 01:18:29 +0700

    background.js

---
+++ cell03/ex00/background.js
@@ -0,0 +1,19 @@
+const button = document.getElementById("colorButton");
+
+button.addEventListener("click", function () {
+
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    const color =
+        "rgb(" +
+        red +
+        "," +
+        green +
+        "," +
+        blue +
+        ")";
+
+    document.body.style.backgroundColor = color;
+});
